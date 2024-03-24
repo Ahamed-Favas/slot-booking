2be56b2f45791ffa054a27443a06f250d2153841
Ahamed-Favas <ahamedfavasedanatt@gmail.com>
2024-03-24 19:14:37 +0530

    added code

--- app/main/page.jsx
+++ app/main/page.jsx
@@ -1,8 +1,5 @@
-"use server"
 import MainPage from "@/components/MainPage"
-// import { authOptions } from "../api/auth/[...nextauth]/route";
-// import { getServerSession } from "next-auth";
-import { unstable_noStore } from "next/cache";
+
 async function getData() {
     const jobs_res = await fetch(process.env.NEXTAUTH_URL + '/api/getWork', {cache:"no-store"});
     const { works } = await jobs_res.json();
@@ -12,10 +9,7 @@ async function getData() {
     return initialData
 }
 
-// const session = await getServerSession(authOptions);
-
 export default async function page() {
-  unstable_noStore()
   const initialData = await getData()
   return(
     <div><MainPage initialData={initialData} /></div>
