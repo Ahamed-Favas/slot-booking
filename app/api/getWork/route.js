da3bddf49e1f3b4bf4312096aa746a1793f4f9f8
Ahamed-Favas <ahamedfavasedanatt@gmail.com>
2024-03-24 20:33:00 +0530

    added code

--- app/api/getWork/route.js
+++ app/api/getWork/route.js
@@ -1,10 +1,9 @@
 import { connectMongoDB } from "@/lib/mongodb"
 import Work from "@/models/work";
 import { NextResponse } from "next/server"
-import { unstable_noStore } from "next/cache";
+
 export async function GET(){
     try {
-        unstable_noStore()
         await connectMongoDB();
         const works = await Work.find()
         return NextResponse.json({works},{status:200})
