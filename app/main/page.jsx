eec00b0ca3f6b13e35beb4d9dba60069ff557145
Ahamed-Favas <ahamedfavasedanatt@gmail.com>
2024-03-24 20:44:01 +0530

    added code

--- app/main/page.jsx
+++ app/main/page.jsx
@@ -1,17 +1,28 @@
-import MainPage from "@/components/MainPage"
+// import MainPage from "@/components/MainPage"
 
-async function getData() {
-    const jobs_res = await fetch(process.env.NEXTAUTH_URL + '/api/getWork', {cache:"no-store"});
-    const { works } = await jobs_res.json();
-    let initialData = []
-    if(works){
-     initialData = (works.slice().reverse())}
-    return initialData
-}
+// async function getData() {
+//     const jobs_res = await fetch(process.env.NEXTAUTH_URL + '/api/getWork', {cache:"no-store"});
+//     const { works } = await jobs_res.json();
+//     let initialData = []
+//     if(works){
+//      initialData = (works.slice().reverse())}
+//     return initialData
+// }
 
-export default async function page() {
-  const initialData = await getData()
-  return(
-    <div><MainPage initialData={initialData} /></div>
+// export default async function page() {
+//   const initialData = await getData()
+//   return(
+//     <div><MainPage initialData={initialData} /></div>
+//   )
+// }
+
+import Hello from "@/components/Hello";
+
+
+function page() {
+  return (
+    <div><Hello/></div>
   )
 }
+
+export default page
---
+++ components/Hello.jsx
@@ -0,0 +1,7 @@
+function Hello() {
+  return (
+    <div>Hello</div>
+  )
+}
+
+export default Hello
